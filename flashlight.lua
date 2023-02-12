679f01df35fb0a4ae6d3b4e45428f9437586c900
Evain Mathis <mathis.evain@epitech.eu>
2023-02-12 17:05:39 +0100

    Try to merge, Big T come help me

---
+++ flashlight.lua
@@ -0,0 +1,56 @@
+flashlight = {}
+
+local blackShader = love.graphics.newShader([[
+    vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
+        return vec4(0, 0, 0, 0.9436);
+    }
+]])
+
+function flashlight:load(x, y)
+
+    light = {}
+    light.x = x
+    light.y = y
+    light.lightImage = love.graphics.newImage("assets/light.png")
+    light.isOn = true
+    light.angle = 0
+    light.isThundering = false
+    return
+end
+
+
+function flashlight:draw()
+    if (light.isOn) then
+        love.graphics.stencil(function()
+            love.graphics.draw(light.lightImage, light.x, light.y, light.angle, 4, 1, 0, light.lightImage:getHeight() / 2)
+        end, "replace", 1)
+        love.graphics.setStencilTest("equal", 0)
+    elseif (light.isThundering == false) then
+        love.graphics.setColor(0, 0, 0, 1)
+        love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+        love.graphics.setColor(1, 1, 1, 1)
+    end
+    if (light.isThundering == false) then
+        love.graphics.setShader(blackShader)
+        love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+        love.graphics.setShader()
+        love.graphics.setStencilTest()
+    end
+    light.isThundering = false
+end
+
+
+function flashlight:update(dt)
+    local mouseX, mouseY = love.mouse.getPosition()
+    light.angle = math.atan2(mouseY - light.y, mouseX - light.x)
+    light.x = love.graphics.getWidth() / 2
+    light.y = love.graphics.getHeight() / 2
+end
+
+function flashlight:thunder()
+    light.isThundering = true
+end
+
+function flashlight:toggle()
+    light.isOn = not light.isOn
+end
--- main.lua
+++ main.lua
@@ -1,19 +1,33 @@
-require "inventory"
 require "collision"
+require "flashlight"
 
 function love.load()
     love.window.setMode(1920, 1080)
     love.window.setTitle("Servietsky")
     -- inventory.load()
     collision.load()
+    flashlight.load()
 end
 
 function love.update(dt)
     -- inventory.update(dt)
     collision.update(dt)
+    flashlight:update(dt)
 end
 
 function love.draw()
     -- inventory.draw()
     collision.draw()
+    flashlight.draw()
+    love.graphics.draw(background, -500, -600)
+    flashlight:draw()
+end
+
+function love.keypressed(key)
+    if key == "space" then
+        flashlight:toggle()
+    end
+    -- if key == "t" then
+    --     flashlight:thunder()
+    -- end
 end
