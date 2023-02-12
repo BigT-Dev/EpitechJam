4d9656a8b691c67495d54085288fed96865dced5
fanfouette <sasha.dupeyroux@epitech.eu>
2023-02-12 17:22:46 +0100

    [UPDATE] old files that had been changed for a clearer, more readable and optimized code

--- main.lua
+++ main.lua
@@ -1,15 +1,35 @@
-require "inventory"
+require("src/range")
+require("src/inventory")
+require("src/items/deo")
+require("src/items/monster")
+require("src/items/items")
+require("src/items/rtx")
+require("src/items/nivea")
+require("src/items/ammo")
+require("src/items/nerf")
 
 function love.load()
     love.window.setMode(1920, 1080)
+    love.window.setTitle("Inventory")
+    love.graphics.setBackgroundColor(21, 21, 21)
+    range.load()
     inventory.load()
 end
 
 function love.update(dt)
-    inventory.update(dt)
+    range.update(dt)
+    items.update(dt)
+
+    if love.keyboard.isDown("c") then
+        inventory.update(dt)
+    end
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor(155, 0, 0)
-    inventory.draw()
+    range.draw()
+    items.draw()
+    love.graphics.setColor(50, 50, 50)
+    if love.keyboard.isDown("c") then
+        inventory.draw()
+    end
 end
